f695c7535487468adb8cf8ed135ad9881775924b
hezijian6338 <627222344@qq.com>
2019-02-18 16:55:09 +0800

    调整好tree的第一元素的逻辑

--- src/page/admin/addTree.vue
+++ src/page/admin/addTree.vue
@@ -1,25 +1,92 @@
 <template>
   <div>
     <van-cell-group>
-      <van-field
-        v-model="this.tree_s1.k"
-        required
-        clearable
-        label="规格名称"
-        right-icon="question"
-        placeholder="请输入规格名称"
-        @click-right-icon="$toast('question')"
-      />
+      <div>
+        <van-field
+          v-model="this.tree_s1.k"
+          required
+          clearable
+          label="规格名称(主)"
+          right-icon="question"
+          placeholder="请输入规格名称(例如:口味)"
+          @click-right-icon="$toast('question')"
+        />
+      </div>
+      <div v-for="(tree, index) in v" :key="index">
+        <van-swipe-cell :right-width="65" :left-width="65" style="margin-top:10px">
+          <van-button
+            type="warning"
+            slot="left"
+            @click="clearTree_name(index)"
+            :block="true"
+            style="margin:0 auto"
+          >清空</van-button>
+          <van-field
+            v-model="tree.name"
+            :label="tree_v_name(index)"
+            placeholder="请输入规格详情(例如:抹茶)"
+            required
+            clearable
+          />
+          <!-- <span slot="right">删除</span> -->
+          <van-button
+            type="danger"
+            slot="right"
+            @click="v.splice(index, 1)"
+            :block="true"
+            style="margin:0 auto"
+          >删除</van-button>
+        </van-swipe-cell>
 
-      <van-field v-model="price" label="价格" placeholder="请输入价格" required clearable/>
-      <van-field v-model="imageURL" label="图片" placeholder="请上传图片" required clearable>
-        <!-- <van-button slot="button" size="small" type="primary">发送验证码</van-button> -->
-      </van-field>
-      <van-uploader :after-read="onRead" accept="image/gif, image/jpeg">
-        <van-icon v-show="upload" name="photograph"/>
-        <img ref="photograph" style="width: 200px">
-      </van-uploader>
+        <van-swipe-cell :right-width="65" :left-width="65">
+          <van-button
+            type="warning"
+            slot="left"
+            @click="clearTree_imgUrl(index)"
+            :block="true"
+            style="margin:0 auto"
+          >清空</van-button>
+
+          <van-field
+            v-model="tree_v.imgUrl"
+            :label="tree_v_imgUrl(index)"
+            placeholder="请左向滑动来上传图片"
+            required
+            clearable
+            disabled
+          />
+          <van-uploader slot="right" :after-read="onRead" accept="image/gif, image/jpeg">
+            <van-icon v-show="upload" name="photograph" size="40px"/>
+            <!-- <img ref="photograph" style="width: 200px"> -->
+          </van-uploader>
+        </van-swipe-cell>
+      </div>
     </van-cell-group>
+    <van-swipe-cell :right-width="65" :left-width="85">
+      <van-button
+        type="danger"
+        slot="left"
+        @click="continue_add"
+        :block="true"
+        style="margin:0 auto"
+      >继续添加</van-button>
+      <div style="text-align:center">
+        <van-icon
+          slot="icon"
+          :color="icon_color"
+          :name="icon_name"
+          size="30px"
+          @click="control_Tree"
+        />
+      </div>
+      <van-button
+        type="primary"
+        slot="right"
+        @click="finish_add"
+        :block="true"
+        style="margin:0 auto"
+      >完成</van-button>
+    </van-swipe-cell>
   </div>
 </template>
 
@@ -30,6 +97,14 @@ export default {
   data() {
     return {
       upload: true,
+      icon_name: "add-o",
+      icon_color: "",
+      v: [
+        {
+          imgUrl: "",
+          name: ""
+        }
+      ],
       tree_v: {
         imgUrl: "",
         name: ""
@@ -79,13 +154,53 @@ export default {
         .catch(() => {
           // on cancel
         });
+    },
+    control_Tree() {
+      if (this.icon_name == "add-o") {
+        this.v.push(this.tree_v);
+      }
+      if (this.icon_name == "passed") {
+        this.$dialog.confirm({
+          title: "再次确认框",
+          message: "是否确认此页资料?"
+        });
+      }
+    },
+    clearTree_name(index) {
+      this.v[index].name = "";
+    },
+    clearTree_imgUrl(index) {
+      this.v[index].imgUrl = "";
+    },
+    tree_v_name(index) {
+      return "规格详情-" + (index + 1);
+    },
+    tree_v_imgUrl(index) {
+      return "图片-" + (index + 1);
+    },
+    finish_add() {
+      this.icon_name = "passed";
+      this.icon_color = "green";
+    },
+    continue_add() {
+      this.icon_name = "add-o";
+      this.icon_color = "red";
     }
   }
 };
 </script>
 <style>
 .div {
-  padding: 1em;
+  padding: 1.5em;
+}
+.van-icon-add-o {
+  transition: transform 2s;
+  -moz-transition: transform 2s; /* Firefox 4 */
+  -webkit-transition: transform 2s; /* Safari 和 Chrome */
+  -o-transition: transform 2s; /* Opera */
+}
+.van-icon-add-o:active {
+  -webkit-transform: scale(5);
 }
 </style>
 
